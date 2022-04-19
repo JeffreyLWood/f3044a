489cd993ed8d05c34d5434a0dcab70dd37b2d708
Jeffrey Wood <jwfirenze@gmail.com>
2022-04-19 12:20:00 -0400

    conversations in order, messages not in order

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -19,9 +19,12 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "ASC"]],
+      order: [[Message, "createdAt", "DESC"]],
       include: [
-        { model: Message, order: ["createdAt", "DESC"] },
+        {
+          model: Message,
+          order: [Conversation, Message, ["createdAt", "ASC"]],
+        },
         {
           model: User,
           as: "user1",
@@ -68,7 +71,7 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length-1].text;
+      convoJSON.latestMessageText = convoJSON.messages[0].text;
       conversations[i] = convoJSON;
     }
 
