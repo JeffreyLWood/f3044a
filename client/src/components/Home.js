a940266edd4062c70d2d83507bf87d2ea66ab096
Jeffrey Wood <jwfirenze@gmail.com>
2022-04-22 13:57:21 -0400

    code removed

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import { Grid, CssBaseline, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-
 import { SidebarContainer } from '../components/Sidebar';
 import { ActiveChat } from '../components/ActiveChat';
 import { SocketContext } from '../context/socket';
