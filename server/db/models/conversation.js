26645f67dbd411029dd9b25ca7de9dbdd339d52d
Jeffrey Wood <jwfirenze@gmail.com>
2022-04-24 16:54:19 -0400

    find conversation based on ids

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -6,20 +6,32 @@ const Conversation = db.define("conversation", {});
 
 // find conversation given two user Ids
 
-Conversation.findConversation = async function (user1Id, user2Id) {
-  const conversation = await Conversation.findOne({
-    where: {
-      user1Id: {
-        [Op.or]: [user1Id, user2Id]
-      },
-      user2Id: {
-        [Op.or]: [user1Id, user2Id]
+Conversation.findConversation = async function (userIds) {
+  const conversations = await User.findOne({
+    where: { id: [userIds], include: Conversation },
+  });
+
+  let table = {};
+  let numberOfUsers = userIds.length;
+
+  const findConversation = (userIds) => {
+    for (let i = 0; i < conversations.length; i++) {
+      if (userIds[userIds.length - 1] === conversations[i].userId) {
+        table[conversations.id] = conversations[i].userId;
+      }
+      if (table[conversations[i].id].length === numberOfUsers) {
+        return table[conversations.id];
+      } else {
+        userIds.splice(userIds.length - 1, 1);
+        findConversation(userIds);
       }
     }
-  });
+  };
 
   // return conversation or null if it doesn't exist
   return conversation;
 };
 
 module.exports = Conversation;
+
+// [username: 'jeff', conversations: [{id:1, userId:1}, {id:1, userId: 5}, {id:1, userId: 6}, {id:2, userId: 1}]
