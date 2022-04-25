137a8fa17c59c1e4b6a3c66b5969b3b7f482fc2a
Jeffrey Wood <jwfirenze@gmail.com>
2022-04-25 18:23:06 -0400

    switch

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -8,23 +8,27 @@ const Conversation = db.define("conversation", {});
 // find conversation given two user Ids
 
 Conversation.findConversation = async function (userIds) {
-  let conversations = [];
-  let table = {};
-  //Put all conversations by each user in userIds in an array
-  userIds.map(async (id) => {
-    table[id] = true;
-    let data = await User.findByPk(id, { include: Conversation });
-    conversations.push(data.conversations);
-  });
-  //Loop through conversations, if conversations[i].userId is not one of the userIds, remove it from the array
-  for (let i = 0; i < conversations.length; i++) {
-    if (!conversations[i].userId.in(table)) {
-      conversations.splice(i, 1);
-    }
-  }
-
-  // return conversation or null
-  return conversations[0] || null;
+  let conversations = await User.findByPk(id, { include: Conversation });
+  console.log(conversations);
+  return conversations[0];
 };
 
 module.exports = Conversation;
+
+// let conversations = [];
+// let table = {};
+// //Put all conversations by each user in userIds in an array
+// userIds.map(async (id) => {
+//   table[id] = true;
+//   let data = await User.findByPk(id, { include: Conversation });
+//   conversations.push(data.conversations);
+// });
+// //Loop through conversations, if conversations[i].userId is not one of the userIds, remove it from the array
+// for (let i = 0; i < conversations.length; i++) {
+//   if (!conversations[i].userId.in(table)) {
+//     conversations.splice(i, 1);
+//   }
+// }
+
+// // return conversation or null
+// return conversations[0] || null;
